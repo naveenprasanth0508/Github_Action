af1ea2c3c38bdd0e81ad8efd671f5d81ec2c8bea
NAVEEN PRASANTH P <145205199+naveenprasanth0508@users.noreply.github.com>
2025-08-07 11:22:04 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from addition import add
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     result = add(5, 7)
-    return f"✅ The sum of 5 and 7 is: {result}"
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
